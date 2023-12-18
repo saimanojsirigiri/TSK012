908590e697fef26ceb898594579521ebafbfb606
saimanojsirigiri <152248557+saimanojsirigiri@users.noreply.github.com>
2023-12-18 11:07:50 +0530

    Changes

--- Database/Questions.database.js
+++ Database/Questions.database.js
@@ -25,17 +25,6 @@ const questions = new schema({
   },
 });
 
-// questions.pre("save", async function (next) {
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     const hashAnswer = await bcrypt.hash(this.ans, salt);
-//     this.ans = hashAnswer;
-//     next();
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
 const Ques = mongoose.model("ques", questions);
 
 module.exports = Ques;
