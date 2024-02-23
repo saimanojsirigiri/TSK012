0a38f2c2fa32af08a2c513b2ecca1a7ffc45d19c
saimanojsirigiri <saimanojsirigiri@gmail.com>
2024-02-23 16:35:27 +0530

    commit

--- app.js
+++ app.js
@@ -11,34 +11,34 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 const PORT = process.env.PORT || 3031;
 
-app.get("/", async (req, res, next) => {
-  res.send("Hello");
+app.get("/", async(req, res, next) => {
+    res.send("Hi");
 });
 
 app.use("/api", route);
 
-app.use(async (req, res, next) => {
-  next(createError.NotFound("Url doesnt exist"));
+app.use(async(req, res, next) => {
+    next(createError.NotFound("Url doesnt exist"));
 });
 
-app.use(async (err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    error: {
-      status: err.status || 500,
-      message: "Question not found",
-    },
-  });
-  console.log(err);
+app.use(async(err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: "Question not found",
+        },
+    });
+    console.log(err);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
