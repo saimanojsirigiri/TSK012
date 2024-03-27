530abd3c7e151945ac4063efede29edd7fa41a47
saimanojsirigiri <saimanojsirigiri@gmail.com>
2024-03-27 13:02:38 +0530

    true

--- Routing/questions.js
+++ Routing/questions.js
@@ -10,76 +10,79 @@ const bcrypt = require("bcrypt");
 
 const crudOperations = generateCrudOperations(ques);
 
-route.get("/fetchAllQuestions", async (req, res, next) => {
-  crudOperations
-    .getAll()
-    .then((data) => res.send(data))
-    .catch((err) => next(err));
+route.get("/fetchAllQuestions", async(req, res, next) => {
+    crudOperations
+        .getAll()
+        .then((data) => res.send(data))
+        .catch((err) => next(err));
 });
 
-route.get("/fetchByTopic/:Topic", async (req, res, next) => {
-  const topic = req.params.Topic;
-  crudOperations
-    .getByTopic(topic)
-    .then((data) => res.send(data))
-    .catch((err) => next(err));
+route.get("/fetchByTopic/:Topic", async(req, res, next) => {
+    const topic = req.params.Topic;
+    crudOperations
+        .getByTopic(topic)
+        .then((data) => res.send(data))
+        .catch((err) => next(err));
 });
 
-route.get("/randomQuestion", async (req, res, next) => {
-  try {
-    const unAnsweredQuestions = await ques.find({ isAnswered: false });
-    if (unAnsweredQuestions.length === 0) {
-      res.status(404).json({ error: "No unanswered questions found" });
-    }
-
-    const randomIndex = Math.floor(Math.random() * unAnsweredQuestions.length);
-
-    const randomQuestion = unAnsweredQuestions[randomIndex];
-
-    console.log("Random ID is: " + randomQuestion);
-
-    ques
-      .findByIdAndUpdate(
-        { _id: randomQuestion._id },
-        {
-          $set: {
-            isAnswered: true,
-          },
+route.get("/randomQuestion", async(req, res, next) => {
+    try {
+        const unAnsweredQuestions = await ques.find({ isAnswered: false });
+        if (unAnsweredQuestions.length === 0) {
+            res.status(404).json({ error: "No unanswered questions found" });
         }
-      )
-      .then((data) => res.json(data))
-      .catch((err) => res.status(500).json({ message: err.message }));
-    // res.json({ randomQuestion });
-  } catch (err) {
-    next(err);
-  }
+
+        const randomIndex = Math.floor(Math.random() * unAnsweredQuestions.length);
+
+        const randomQuestion = unAnsweredQuestions[randomIndex];
+
+        console.log("Random ID is: " + randomQuestion);
+
+        ques
+            .findByIdAndUpdate({ _id: randomQuestion._id }, {
+                $set: {
+                    isAnswered: true,
+                },
+            })
+            .then((data) => {
+                if (data) {
+                    res.json(data);
+                } else {
+                    res.json({ message: "No random question" });
+                }
+            })
+            .catch((err) => res.status(500).json({ message: err.message }));
+        // res.json({ randomQuestion });
+    } catch (err) {
+        next(err);
+    }
 });
 
-route.post("/postData", async (req, res, next) => {
-  const arrayData = Array.isArray(req.body) ? req.body : [req.body];
-  try {
-    const savedQuestions = await Promise.all(
-      arrayData.map(async (questionData) => {
-        const { que, opt, ans, topic, isAnswered, timeLimit } = questionData;
-        const hashedAns = await bcrypt.hash(ans, 10);
+route.post("/postData", async(req, res, next) => {
+    const arrayData = Array.isArray(req.body) ? req.body : [req.body];
+    try {
+        const savedQuestions = await Promise.all(
+            arrayData.map(async(questionData) => {
+                const { que, opt, ans, topic, isAnswered, timeLimit } = questionData;
+                const hashedAns = await bcrypt.hash(ans, 10);
 
-        const newQuestion = new ques({
-          que,
-          opt,
-          ans: hashedAns,
-          topic,
-          isAnswered,
-          timeLimit,
-        });
-        return newQuestion.save();
-      })
-    );
-    res
-      .status(201)
-      .json({ message: "Questions Saved Successfully", ques: savedQuestions });
-  } catch (err) {
-    next(err);
-  }
+                const newQuestion = new ques({
+                    que,
+                    opt,
+                    ans: hashedAns,
+                    topic,
+                    isAnswered,
+                    timeLimit,
+                });
+                return newQuestion.save();
+            })
+        );
+        res
+            .status(201)
+            .json({ message: "Questions Saved Successfully", ques: savedQuestions });
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = route;
